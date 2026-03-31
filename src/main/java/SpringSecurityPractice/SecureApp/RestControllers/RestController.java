716c8fe0984691692d26cc9f001c3ba2be841016
Alec <alecjames1996@aol.com>
2026-03-31 15:15:33 -0400

    Set up the following:
    DB connection to postgres DB
    Made a test endpoint

--- src/main/java/SpringSecurityPractice/SecureApp/RestControllers/RestController.java
+++ src/main/java/SpringSecurityPractice/SecureApp/RestControllers/RestController.java
@@ -1,4 +1,23 @@
 package SpringSecurityPractice.SecureApp.RestControllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@org.springframework.web.bind.annotation.RestController
+
+
+@RequestMapping("/security-practice")
 public class RestController {
+
+    @GetMapping("/hello")
+    public String helloEndpoint(@RequestParam(required = false) String name) {
+
+        if (name == null) {
+            return "Hello Guest" ;
+        }
+
+        return "Hello " + name;
+    }
+
 }
