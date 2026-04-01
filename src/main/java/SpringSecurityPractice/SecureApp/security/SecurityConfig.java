a47c24f02be7681d27d657b9665d385e6a37aad4
Alec <alecjames1996@aol.com>
2026-03-31 23:08:14 -0400

    Added SecurityFilterChain

--- src/main/java/SpringSecurityPractice/SecureApp/security/SecurityConfig.java
+++ src/main/java/SpringSecurityPractice/SecureApp/security/SecurityConfig.java
@@ -2,12 +2,15 @@ package SpringSecurityPractice.SecureApp.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
@@ -28,6 +31,19 @@ public class SecurityConfig {
     @Bean
     PasswordEncoder passwordEncoder() {
         return NoOpPasswordEncoder.getInstance();
+    }
+
+    @Bean
+    SecurityFilterChain configure(HttpSecurity http) throws Exception {
+
+        http.csrf(csrf -> csrf.disable())
+                .httpBasic(Customizer.withDefaults())
+                .authorizeHttpRequests(auth -> auth
+                        .anyRequest().authenticated()
+                );
+
+        return http.build();
+
     }
 
 }
