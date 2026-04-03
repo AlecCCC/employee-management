1cf483692897d80e35c07d068eb4955e72c03cf8
Alec <alecjames1996@aol.com>
2026-04-03 00:41:51 -0400

    Working on DB authentication

--- src/main/java/SpringSecurityPractice/SecureApp/security/SecurityConfig.java
+++ src/main/java/SpringSecurityPractice/SecureApp/security/SecurityConfig.java
@@ -16,34 +16,5 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
 
 
-    // The app now uses this instance and overrides default no users exist yet.
-
-    @Bean
-    UserDetailsService userDetailsService() {
-
-        UserDetails user = User.withUsername("Alec")
-                .password("123")
-                .authorities("USER")
-                .build();
-        return new InMemoryUserDetailsManager(user);
-    }
-
-    @Bean
-    PasswordEncoder passwordEncoder() {
-        return NoOpPasswordEncoder.getInstance();
-    }
-
-    @Bean
-    SecurityFilterChain configure(HttpSecurity http) throws Exception {
-
-        http.csrf(csrf -> csrf.disable())
-                .httpBasic(Customizer.withDefaults())
-                .authorizeHttpRequests(auth -> auth
-                        .anyRequest().authenticated()
-                );
-
-        return http.build();
-
-    }
 
 }
--- src/main/java/SpringSecurityPractice/repo/UserRepo.java
+++ src/main/java/SpringSecurityPractice/repo/UserRepo.java
@@ -1,4 +1,13 @@
 package SpringSecurityPractice.repo;
 
-public interface UserRepo {
+import SpringSecurityPractice.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+import java.util.Optional;
+
+public interface UserRepo extends JpaRepository<Employee, Long> {
+
+    Optional<Employee> findByUsername(String username);
+
 }
